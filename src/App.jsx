be45f12114d5c8f92cdd11274d1f8976080750a6
llynoirx <kathyho04@gmail.com>
2024-09-27 18:27:24 -0400

    checkpoint 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
 
   return (
     <div className="App">
-
+      <h1>Samosa Selector</h1>
+      <h2>Count:</h2>
+      <img className='samosa' src='../public/images/samosa.png' />
     </div>
   )
 }
